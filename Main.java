53eaf010b8c5a0b0fae1f7824231aa962566f50f
Denny29 <43240668+Denny29@users.noreply.github.com>
2022-04-15 22:42:09 -0400

    Update Main.java
    Now you can replay the game.

--- Main.java
+++ Main.java
@@ -4,14 +4,28 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         TicTacToe game = new TicTacToe();
 
-        while(!game.gameOver()){
+        char cont = 'y';
+
+        while(cont == 'y' || cont == 'Y'){
             System.out.println("\n" + game);
             System.out.println(game.getPlayer() + "\nInput where you want to go. (1 - 9)");
 
             int user = scan.nextInt() - 1;
 
             game.play((int) (user / 3), user % 3);
+
+            if(game.gameOver()){
+                System.out.println(game);
+                System.out.println("Play again? (y / n)");
+                cont = scan.next().charAt(0);
+                if(cont == 'y' || cont == 'Y'){
+                    game = new TicTacToe();
+                    //Clears screen
+                    System.out.print("\033[H\033[2J");  
+                    System.out.flush();
+                }
+            }
         }
-        System.out.print(game);
+        
       }
 }
